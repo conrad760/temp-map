430639b0894b697f8c4e82653b43bc58bb5e19d1
Conrad Parker <ConradlParker@gmail.com>
2020-02-08 15:19:51 -0600

    added a dotenv and an example

--- Main.py
+++ Main.py
@@ -1,5 +1,41 @@
+import os
+import sys
+import dotenv
 import plotly.express as px
 import pandas as pd
+import requests
+
+# settings.py
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), './.config/.env')
+load_dotenv(dotenv_path)
+
+# functions
+# def get_apikey(email=None, passwd=None):
+#     if email == None or passwd == None:
+#         email = input("enter your email: ")
+#         passwd = input("enter your password: ")
+
+#     url = "https://app.climate.azavea.com?email={email}&password={passwd}"
+#     payload = {}
+#     headers = {
+#         'Content-Type': 'application/x-www-form-urlencoded'
+#     }
+#     response = requests.request("POST", url, headers=headers, data=payload)
+
+#     # print(response.cookies.get_dict())
+#     #  # doesn't quite work
+
+
+api_key = os.getenv("CLIMATE_API_KEY")
+user_email = os.getenv("CLIMATE_EMAIL")
+user_passwd = os.getenv("CLIMATE_PASSWORD")
+if api_key == None:
+    pass
+    # api_key = get_apikey(user_email, user_passwd)
+print(api_key)
 
 
 fig = px.imshow([[1, 20, 30],
--- README.md
+++ README.md
@@ -3,11 +3,14 @@ This is a visualization project for a climate change weekend workshop. We are us
 
 We will be using [plotly](https://www.plot.ly/python/heatmaps) and Python 3.8 for visualization.
 
+We will be managing more sensative information with a [dotenv](https://pypi.org/project/python-dotenv/) files. check out [.config/.env](./.config/.env) for an example file.
+
+
 ## Getting started 
 
 ``` py
 pip install --user -r requirment.txt
-
+# add what you need to your dotenv file
 python Main.py
 
 ```
